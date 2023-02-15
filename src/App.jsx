14183dfe530a00ae2db05debd9c42970a25aff90
Joseph M <jmedina1985@hotmail.com>
2023-02-15 11:11:43 -0800

    Some notes on Monaco

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  // an important concept to understand about monaco - it will take up the full width/height of it's container (width:100%, height:100%)
+  // you can have editors of any size
+  // full screen or embed type size
+
   return (
     <div className="App">
 
