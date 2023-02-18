a9d765f527613ab901cb55eb127065b3eb021b03
Joseph M <jmedina1985@hotmail.com>
2023-02-17 21:45:07 -0800

    create choosing file logic

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const files = {
 
 function App() {
   const [fileName, setFileName] = useState("script.py");
+  // files["script.py"] -> name, language, value -> pass those to the editor
 
   // an important concept to understand about monaco - it will take up the full width/height of it's container (width:100%, height:100%)
   // you can have editors of any size
