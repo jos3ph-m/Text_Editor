5c3a904ce911d5c71cc1c1d00bb1829c33c0d6ea
Joseph M <jmedina1985@hotmail.com>
2023-02-21 21:29:14 -0800

    Get ref for Monaco value

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,7 @@ function App() {
     <div className="App">
       <button onClick={() => setFileName("index.html")}>Switch to index.html</button>
       <button onClick={() => setFileName("script.py")}>Switch to script.py</button>
+      <button></button>
       <Editor
       height="100vh"
       width="100%" 
